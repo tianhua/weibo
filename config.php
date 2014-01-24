0fcf47525e174847a757e0f9e0b95bb72c226fd4
tianhua <azheng@SI-AZHENG.corp.valueclick.com>
2014-01-24 14:22:06 +0800

    init

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define( "WB_AKEY" , '3721470456' );
+define( "WB_SKEY" , '293a498eba25332707f0b4b35f2507b3' );
+define( "WB_CALLBACK_URL" , 'http://localhost.weibo.com:8080/weibo/callback.php' );
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+include_once( 'config.php' );
+include_once( 'saetv2.ex.class.php' );
+
+$o = new SaeTOAuthV2( WB_AKEY , WB_SKEY );
+
+$code_url = $o->getAuthorizeURL( WB_CALLBACK_URL );
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta property="wb:webmaster" content="71163f0a2a86dc96" />
+<title>Demo - Powered by Sina App Engine</title>
+</head>
+
+<body>
+
+    <p><a href="<?=$code_url?>"><img src="weibo_login.png" title="ç‚¹å‡»è¿›å…¥æŽˆæ�ƒé¡µé�¢" alt="ç‚¹å‡»è¿›å…¥æŽˆæ�ƒé¡µé�¢" border="0" /></a></p>
+
+</body>
+</html>
