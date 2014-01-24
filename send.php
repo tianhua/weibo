d36cb681540afe4fa38ddba46d19d81bb2e91eb9
tianhua <azheng@SI-AZHENG.corp.valueclick.com>
2014-01-24 17:59:36 +0800

    modify send

--- send.php
+++ send.php
@@ -1,17 +1,16 @@
 <?php
-session_start();
+session_start ();
 
-include_once( 'config.php' );
-include_once( 'saetv2.ex.class.php' );
+include_once ('config.php');
+include_once ('saetv2.ex.class.php');
 
-$o = new SaeTOAuthV2( WB_AKEY , WB_SKEY );
+$o = new SaeTOAuthV2 ( WB_AKEY, WB_SKEY );
 
-$code_url = $o->getAuthorizeURL( WB_CALLBACK_URL );
-if(isset($_POST['submitBtn']))
-{
-	$c = new SaeTClientV2( WB_AKEY , WB_SKEY , $_SESSION['token']['access_token'] );
-	$content = htmlentities($_POST['content']);
-	$ret = $c->update( $content );
+$code_url = $o->getAuthorizeURL ( WB_CALLBACK_URL );
+if (isset ( $_POST ['submitBtn'] )) {
+	$c = new SaeTClientV2 ( WB_AKEY, WB_SKEY, $_SESSION ['token'] ['access_token'] );
+	$content = htmlentities ( $_POST ['content'] );
+	$ret = $c->update ( $content );
 }
 
 ?>
@@ -21,13 +20,61 @@ if(isset($_POST['submitBtn']))
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta property="wb:webmaster" content="71163f0a2a86dc96" />
 <title>Demo - Powered by Sina App Engine</title>
+<style>
+body{max-width:1024px;margin:0 auto;}
+.clear {
+	clear: both;
+}
+
+.input {
+	margin-top: 5px;
+}
+
+.input .lable {
+	float: left;
+	font-weight: bold;
+	text-align: right; margin-right : 10px;
+	min-width: 200px;
+	min-width: 200px;
+	margin-right: 10px;
+}
+
+.input .field {
+	float: left;
+}
+</style>
 </head>
 
 <body>
-<form method="post">
-<textarea name='content' rows="" cols=""></textarea>
-<input name='submitBtn' type = 'submit' value='submit'></input>
-</form>
-    
+	<form method="post">
+		<div class='input'>
+			<div class='lable'>title</div>
+			<div class='field'>
+				<input type='text' name='title'></input>
+			</div>
+			<div class='clear'></div>
+		</div>
+		<div class='input'>
+			<div class='lable'>send to</div>
+			<div class='field'>
+				<label>sina<input type='checkbox' name='sina'></input></label> <label>renren
+					<input type='checkbox' name='renren'></input>
+				</label>
+			</div>
+			<div class='clear'></div>
+		</div>
+		<div class='input'>
+			<div class='lable'>content</div>
+			<div class='field'>
+				<textarea name='content' rows="15" cols="70"></textarea>
+			</div>
+			<div class='clear'></div>
+		</div>
+		<div style='text-align:center;'>
+			<input name='submitBtn' type='submit' value='submit'></input>
+		</div>
+
+	</form>
+
 </body>
 </html>
