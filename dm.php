f38a40010e7429016d3755e3c732bff030de076a
tianhua <azheng@SI-AZHENG.corp.valueclick.com>
2014-01-29 17:46:12 +0800

    fix getfriends

--- dm.php
+++ dm.php
@@ -8,13 +8,13 @@ $o = new SaeTOAuthV2 ( WB_AKEY, WB_SKEY );
 
 $code_url = $o->getAuthorizeURL ( WB_CALLBACK_URL );
 	$c = new SaeTClientV2 ( WB_AKEY, WB_SKEY, $_SESSION ['token'] ['access_token'] );
-	$content = htmlentities ( $_POST ['content'] );
-	$uid = $c->get_uid();
-	$user = $c->show_user_by_id($uid);
 	
-	var_dump($user);
-	$ret = $c->friends_ids_by_id ( $uid,0,5 );
-	//$ret = $c->friends_by_id ( $uid,0,5 );
+	$uids = $c->get_uid();
+	$uid = $uids['uid'];
+	$user = $c->show_user_by_id($uid);
+
+	//$ret = $c->friends_ids_by_id ( $uid,0,5 );
+	$ret = $c->friends_by_id ( $uid,0,5 );
 	var_dump($ret);
 
 
@@ -22,6 +22,7 @@ $code_url = $o->getAuthorizeURL ( WB_CALLBACK_URL );
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'></script>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta property="wb:webmaster" content="71163f0a2a86dc96" />
 <title>Demo - Powered by Sina App Engine</title>
@@ -48,10 +49,36 @@ body{max-width:1024px;margin:0 auto;}
 	float: left;
 }
 </style>
+<script>
+$(document).ready(function(){
+	$.ajax(
+			{
+				url:'https://api.weibo.com/2/friendships/groups.json',
+				type:'GET',
+				data:{
+					access_token 	: '<?php echo $_SESSION ['token'] ['access_token'];?>'
+						},
+				success: function(  data,  textStatus,  jqXHR ){
+					alert(data);
+					}
+				
+				}
+			);
+});</script>
 </head>
 
 <body>
 	<form method="post">
+	<div class='input'>
+			<div class='lable'>Group</div>
+			<div class='field'>
+				<select name='group'>
+				<option value='all'>All</option>
+				
+				</select>
+			</div>
+			<div class='clear'></div>
+		</div>
 		<div class='input'>
 			<div class='lable'>title</div>
 			<div class='field'>
